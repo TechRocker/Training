a8fac9d75a7104b46039d1debd7cae9e0d185d95
TechRocker <arupghosh_it@hotmail.com>
2017-05-14 16:31:31 +0530

    Added Workers

---
+++ DesignPatternExample/IWorker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternExample
+{
+    public interface IWorker
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+        string Number { get; set; }
+        eWorkerType Type { get; set; }
+
+        void Create(IWorker worker);
+        void Update(IWorker worker);
+        void Delete(IWorker worker);
+    }
+
+    public enum eWorkerType
+    {
+        Knitter=01,
+        Dyer =02,
+        Cutter=03,
+        Sticher=04
+    }
+}
---
+++ DesignPatternExample/Stitcher.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternExample
+{
+    class Stitcher : IWorker
+    {
+        public string Address
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Number
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public eWorkerType Type
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Create(IWorker worker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IWorker worker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(IWorker worker)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
